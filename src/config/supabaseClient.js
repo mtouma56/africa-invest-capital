82048707807339eadada908e7fec8556fb4e350c
mtouma56 <mtouma56@gmail.com>
2025-06-01 18:47:07 +0000

    fix: remove process check in supabaseClient.js for Next.js

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -4,11 +4,6 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error(
-    'Supabase environment variables NEXT_PUBLIC_SUPABASE_URL and NEXT_PUBLIC_SUPABASE_ANON_KEY must be provided'
-  );
-}
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
