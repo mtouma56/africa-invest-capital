e2133fb8516859989464276cf647e73c04a2bd09
mtouma56 <mtouma56@gmail.com>
2025-06-01 12:12:47 +0000

    fix: resolve lint build errors

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -4,8 +4,8 @@ import { createClient } from '@supabase/supabase-js';
 let supabase = null;
 
 if (typeof window !== 'undefined') {
-  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+  const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+  const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
   supabase = createClient(supabaseUrl, supabaseAnonKey);
 }
 
