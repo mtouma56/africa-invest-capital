a2f26696c4ce3016be767e531e2439564f6f212c
mtouma56 <mtouma56@gmail.com>
2025-06-28 08:52:58 +0000

    Update supabaseClient.js

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -3,7 +3,6 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
 
-// Ce message apparaîtra seulement dans le navigateur
 if (typeof window !== 'undefined' && (!supabaseUrl || !supabaseAnonKey)) {
   console.error(
     'Supabase credentials are missing. Check NEXT_PUBLIC_SUPABASE_URL and NEXT_PUBLIC_SUPABASE_ANON_KEY in your environment variables.'
